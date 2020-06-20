5f4218ee394286e675b134cf50f8be03811371a6
akosuawiafe01 <wiafenyarkoa@gmail.com>
2020-06-20 12:54:15 +0000

    Restarting App Clone

--- Components/HomeCarousel.js
+++ Components/HomeCarousel.js
@@ -6,9 +6,28 @@ import HomeCarouselItem from './HomeCarouselItem'
 
 const { width, height } = Dimensions.get('window')
 
-const HomeCarousel = ({  data }) => {
+const renderList  = ({data}) => {
+    return(
+    Object.entries(data).keys
+    )
+}
+
+
+const HomeCarousel = ({ data }) => {
     return (
         <View style={{paddingLeft: 5}}>
+
+
+            <FlatList
+            data={renderList}
+            key={Object.entries(data).keys}
+            renderItem={({item}) => 
+            {return(
+            <HomeCarouselItem item={item} />
+            )}
+            }
+
+            />
 
         
          {/* { Object.entries(data)
